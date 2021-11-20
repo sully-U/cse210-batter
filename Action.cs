6f138b3228dc5c2754f93f789cfba66a066ec568
sully-U <91569218+sully-U@users.noreply.github.com>
2021-11-20 15:15:24 -0700

    Add files via upload

---
+++ Action.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using cse210_batter_csharp.Casting;
+
+namespace cse210_batter_csharp
+{
+    /// <summary>
+    /// The base class of all other actions.
+    /// </summary>
+    public abstract class Action
+    {
+        public virtual void Execute(Dictionary<string, List<Actor>> cast)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
